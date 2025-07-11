b5e69b49eced58fcf81bb92f8887c5651d01a813
andre1gauna <andregauna@hotmail.com>
2025-07-11 00:49:12 -0300

    traversal pre order iterative

---
+++ Python/N-ary Tree Preorder Traversal.py
@@ -0,0 +1,39 @@
+from typing import List, Optional
+
+class Node:
+    def __init__(self, val: Optional[int] = None, children: Optional[List['Node']] = None):
+        self.val = val
+        self.children = children if children is not None else []
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if root is None:
+            return []
+        stack = [root]
+        output = []
+        while stack:
+            temp = stack.pop()
+            output.append(temp.val)
+            stack.extend(temp.children[::-1])
+        return output
+    
+#TODO: Implementar forma recursiva
+
+if __name__ == "__main__":
+    # Cria a árvore:
+    #         1
+    #       / | \
+    #      3  2  4
+    #     / \
+    #    5   6
+
+    node5 = Node(5)
+    node6 = Node(6)
+    node3 = Node(3, [node5, node6])
+    node2 = Node(2)
+    node4 = Node(4)
+    root = Node(1, [node3, node2, node4])
+
+    solution = Solution()
+    result = solution.preorder(root)
+    print(result)  # Esperado: [1, 3, 5, 6, 2, 4]
