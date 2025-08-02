2e53bf10d29a24f83ed35894e725106253631f2c
andre1gauna <andregauna@hotmail.com>
2025-08-02 18:03:44 -0300

    15

---
+++ Python/15 - Design HashSet.py
@@ -0,0 +1,17 @@
+class MyHashSet:
+
+    def __init__(self):
+        self.size = 10**6 + 1
+        self.v = [False] * self.size
+
+    def add(self, key: int) -> None:
+        self.v[key] = True
+
+    def remove(self, key: int) -> None:
+        self.v[key] = False
+        
+
+    def contains(self, key: int) -> bool:
+        return self.v[key]
+    
+#TODO: revisar 11 e 12, repensar 13 e 14
